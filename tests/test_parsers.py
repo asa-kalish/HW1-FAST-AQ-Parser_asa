6a0577f2102b31cdaf5303e35c2d7c62addad0e2
asa-kalish <113955367+asa-kalish@users.noreply.github.com>
2023-01-17 23:37:30 -0800

    Update test_parsers.py

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -5,6 +5,7 @@ from seqparser import (transcribe, reverse_transcribe,
         FastqParser)
 
 import pathlib
+
 def test_freebie_parser_1():
     """
     This one is a freebie
@@ -20,24 +21,16 @@ def test_freebie_parser_2():
     """
     assert 1 != 2
 
+        
 def get_filepath(which):
+        """
+        a method to help retrieve test files
+        """
     data_dir = pathlib.Path(__file__).resolve().parent.parent / "data"
     if which == "fasta":
         return data_dir / "test.fa"
     else:
         return data_dir / "test.fq"
-
-def open_fastq_reference():
-    f = pathlib.Path(__file__).resolve().parent / "fastq-check.txt"
-    with f.open() as f:
-        seqs = list(map(lambda l: l.strip().split("|"), f.readlines()))
-    return seqs  # will be list of lists with seq, quality that were parsed from the test files using get-seq.sh
-
-def open_fasta_reference():
-    f = pathlib.Path(__file__).resolve().parent / "fasta-check.txt"
-    with f.open() as f:
-        seqs = list(map(lambda l: l.strip(), f.readlines()))
-    return seqs  # will be a list of seqs, quality that were parsed from the test files using get-seq.sh
 
 
 def test_FastaParser():
@@ -46,21 +39,29 @@ def test_FastaParser():
     class here. You should generate an instance of
     your FastaParser class and assert that it properly
     reads in the example Fasta File.
+    
+    This might not be the most elegant test, but it automates something 
+    I usually do when looking at new data. I'd look at the data in its
+    raw file, and then print out a line of this file in python to make 
+    sure the file has been read properly. in this case, i will store 
+    the first sequence found in test.fa and then make sure that my 
+    FastaParser class is properly storing that sequence. 
     """
-# i know that seq0 is TGATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA
+
     true_seq = "TGATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA"
         
     fasta_file = get_filepath("fasta")
         
     # Create instance of FastaParser        
-    fasta_parser = FastaParser(fasta_file)
+    fasta_parser = Fa   staParser(fasta_file)
     
+    # iter over FasterParser object, store sequences
     sequences = []
     for seq_name, seq in fasta_parser:
         sequences.append(seq)
-            
+    
+    # just check that seq0 is correct; could be extended to check more cases
     assert sequences[0] == true_seq
-    # pass
 
 
 def test_FastqParser():
@@ -69,18 +70,22 @@ def test_FastqParser():
     class here. You should generate an instance of
     your FastqParser class and assert that it properly
     reads in the example Fastq File.
+    
+    I follow the same logic as in test_FastaParser but include the quality values
     """
     
     # ground truth
     true_seq = "TGTGGTCGTATAGTTATTGTCATAAATTACACAGAATCGCGATTCTCCGCGTCCACCAATCTTAGTGCACCACAGCATCGACCCGATTTATGACGCTGAG"
-                                                                                                      
+    true_quality = '''*540($=*,=.062565,2>'487')!:&&6=,6,*7>:&132&83*8(58&59>'8!;28<94,0*;*.94**:9+7"94(>7='(!5"2/!%"4#32='''                                                                                                  
     fastq_file = get_filepath("fastq")
         
     # Create instance of FastqParser        
     fastq_parser = FastqParser(fastq_file)
     
     sequences = []
+    qualities = []
     for seq_name, seq, quality in fastq_parser:
-        sequences.append(seq)        
+        sequences.append(seq)  
+        qualities.append(quality)
             
-    assert sequences[0] == true_seq
+    assert (sequences[0] == true_seq) and (qualities[0] == true_quality)
